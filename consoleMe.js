3cc11ca9c10a20e2368466ec8dd38f94b17c63b4
fruitloaf <dmitrisanzharov@gmail.com>
2025-02-23 17:33:36 +0000

    all done

--- consoleMe.js
+++ consoleMe.js
@@ -1,7 +1,20 @@
+// consoleMe.js
 function consoleMe(arg){
     console.log('args', arg);
 }
 
+const myVar = 'myVar str';
+
+async function asyncFunc(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('asyncFunc resolved');
+        }, 1000);
+    });
+}
+
 module.exports = {
-    anyKey: consoleMe
+    anyKey: consoleMe,
+    anyKey2: myVar,
+    anyKey3: asyncFunc
 }
--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+// index.js
 const myModule = require('./consoleMe.js');
 myModule.anyKey('hello world');
+
+console.log('myVar', myModule.anyKey2);
+
+myModule.anyKey3().then((res) => {console.log('res', res)})
