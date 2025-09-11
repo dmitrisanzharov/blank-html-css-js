d3c5d69cca05995f8ba064e0308e160e030c9d6e
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-11 12:01:33 +0100

    all done

---
+++ foo.js
@@ -0,0 +1,9 @@
+const foo1 = 'foo1';
+
+function foo2() {
+    return 'foo2';
+}
+
+export { foo1 };
+
+export default foo2;
---
+++ index.js
@@ -0,0 +1,4 @@
+import * as fooData from './foo.js';
+console.log("fooData: ", fooData);
+console.log('default', fooData.default());
+console.log('non default', fooData.foo1);
