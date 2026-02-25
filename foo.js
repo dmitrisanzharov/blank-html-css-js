a4555a9abd1ac270d3a017cdd124e76e0760dacb
fruitloaf <dmitrisanzharov@gmail.com>
2026-02-25 09:27:21 +0000

    a

--- foo.js
+++ foo.js
@@ -1,5 +1,21 @@
-const fooStr1 = 'fooStr1';
+// const fooStr1 = "fooStr1";
 
-module.exports = {
-  fooStr1,
-};
+// async function fooFunc1() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             console.log("fooFunc1 async");
+//             resolve();
+//         }, 3000);
+//     });
+// }
+
+// module.exports = fooStr1;
+
+// static
+// export default 'mah man'  OR export const mahManStr = 'manMan';
+// import anyName from 'path/to/file.js'
+
+// dynamic
+// import ('path/to/file.js').then(module => console.log(module))
+console.log("foo.js is loaded");
+export default 'fooStr1';
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const requireFoo = require('./foo');
-// console.log(requireFoo);
-// console.log(module)
+import axios from 'axios';
+import fooStr1 from './foo.js';
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+    console.log(response.data);
+}).catch(error => {
+    console.error('Error fetching data:', error);
+});
