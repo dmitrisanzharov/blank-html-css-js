f6bd18c12a9125a71eb35261d064288d9879d4a6
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-25 10:36:19 +0100

    all good

---
+++ backup.js
@@ -0,0 +1,38 @@
+// // Constructor Function
+// function Circle(radiusArg){
+//     this.radius = radiusArg;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+
+// let circle2 = new Circle(1);
+// console.log("circle2 constructor: ", circle2.constructor);
+
+// // Factory Function
+// function createCircle(radiusArg){
+//     return {
+//         radius: radiusArg,
+//         draw: function() {
+//             console.log('draw');
+//         }
+//     };
+// }
+
+// let circle = createCircle(1);
+// console.log("circle constructor: ", circle.constructor);
+
+// let x = {};
+// console.log("x constructor: ", x.constructor);
+
+// let y = new Object();
+// console.log("y constructor: ", y.constructor);
+
+// let a = 1; 
+// console.log("a constructor: ", a.constructor);
+
+// let b = 'hello';
+// console.log("b constructor: ", b.constructor);
+
+
+
