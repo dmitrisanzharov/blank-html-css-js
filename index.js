d0b84c44ef5747a815f8ef1a0dac7ec57dd4fecd
VictorSarov <victor.sarov@rentalmatics.com>
2026-09-09 11:19:35 +0100

    all working

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+const root = ReactDOM.createRoot(
+   document.getElementById('root')
+);
 
-const root = createRoot(document.getElementById('root'));
+const element = React.createElement(
+   'h1',
+   null,
+   'Hello React!'
+);
 
-const element = React.createElement('h1', null, 'Hello React!');
 
 root.render(element);
